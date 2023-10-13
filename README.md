a8f6705fcfe1e2f7ee40eb54b516e12904466daf
Dima <d.teyblum@gmail.com>
2023-10-13 13:24:04 +0300

    Transfer to  mac-mini

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # nodered-asterisk-ari
 ## attempts to add StateFull functionality (fork started 11.2022)
+
+
+To start node-red container in DEBUG mode:
+```bash
+docker run --name NodeRed_v3_2022_0_0 -it -p 1880:1880 -p 9229:9229 -v /Users/Dima/PycharmProjects/nodered-asterisk-ari:/data/nodered-asterisk-ari  --entrypoint npm nodered/node-red  run debug_brk -- --userDir /data
+```
--- ari_client.js
+++ ari_client.js
@@ -1,25 +1,223 @@
 var ari = require('ari-client');
-var uuid = require('uuid')
+var uuid = require('uuid');
+
 
 module.exports = function(RED) {
     "use strict";
-    
 
+    var asterisks_ari = {};
+    var stasis_apps ={};
+    class StasisApp{
+        static GetOrCreate(app_name,asterisk_ari, node){
+            if (asterisk_ari.constructor.name !== 'AsteriskAri'){
+                console.error(`Wrong type of parameter in StasisApp.GetOrCreate asteris_ary type: ${type(asterisk_ari)}`);
+                return;
+            }
+            let key=`${app_name}-${asterisk_ari.url}`;
+            if (key in stasis_apps){
+                return stasis_apps[key];
+            } else {
+                return new StasisApp(app_name,asterisk_ari, node);
+            }
+        }
+        constructor(app_name,asterisk_ari,node) {
+            if(asterisk_ari.constructor.name !== 'AsteriskAri'){
+                console.error(`Wrong type of parameter in StasisApp.GetOrCreate asteris_ary type: ${type(asterisk_ari)}`);
+                return;
+            }
+            let key=`${app_name}-${asterisk_ari.url}`;
+            if (key in stasis_apps){
+                console.error(`Application already registred ${name}, ${asterisk_ari.url}`);
+            }
+            this.node = node;
+            node.stasis_app=this;
+            this.app_name = app_name;
+            this.asterisk_ari = asterisk_ari;
+            this.node.status({fill: "red", shape: "dot", text: `Initialising`});
+            this.started = false;
+            this._heardbeat();
+            this._register_app();
+        }
+        _register_app(){
+            if (!this.asterisk_ari.connection_status){
+                this.node.status({fill: "red", shape: "dot", text: `asterisk not connected`});
+                setTimeout(() => {
+                            console.warn(`Can't start App  ${this.app_name} on ${this.asterisk_ari.url} asterisk not connected...`);
+                            this._register_app();
+                        }, 1000);
+                return;
+            }
+            this.asterisk_ari.ari.start(this.app_name).bind(this)
+                .then(function (){
+                    console.log(`Registered App ${this.app_name} on ${this.asterisk_ari.url}`);
+                    this.node.status({fill: "green", shape: "dot", text: `${this.app_name} on ${this.asterisk_ari.url}`});
+                    this.started = true;
+                    this.node.on("close", this._close_app);
+                })
+                .catch(function (err){
+                    console.error(err);
+                    setTimeout(() => {
+//                            console.log(`Trying to reconnect ${url}`);
+                            this._register_app();
+                        }, 1000);
+                });
+        }
+        _heardbeat(){
+            setTimeout(function(){
+                if (this.started) {
+                    this.node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: `${this.app_name} on ${this.asterisk_ari.url} wait for answer...`
+                    });
+
+                    this.connection_status = false;
+                    try {
+                        this.asterisk_ari.ari.applications.filter({applicationName: this.app_name}).bind(this)
+                            .then(function () {
+                                this.node.status({
+                                    fill: "green",
+                                    shape: "dot",
+                                    text: `${this.app_name} on ${this.asterisk_ari.url}`
+                                });
+                                //console.log(`PONG ${application.name} from ${this.asterisk_ari.url}`)
+
+                            })
+                            .catch(function (err) {
+                                this.node.status({
+                                    fill: "red",
+                                    shape: "dot",
+                                    text: `Error: ${this.app_name} on ${this.asterisk_ari.url} ${err}`
+                                });
+                                console.warn(` ${this.app_name} from ${this.asterisk_ari.url} => ${err}`);
+                                this._register_app();
+
+                            });
+
+                    }
+                    catch (err){
+                        this.node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: `${this.app_name} on ${this.asterisk_ari.url} err: ${err}`
+                    });
+                    }
+                } else {
+                    this.node.status({
+                        fill: "red",
+                        shape: "dot",
+                        text: `${this.app_name} on ${this.asterisk_ari.url} wait for starting...`
+                    });
+                }
+                this._heardbeat();
+            }.bind(this),1000);
+        }
+
+        _close_app(){ // TODO THIS in this method are NODE not StasisApp instance why ??
+            console.log(`Close App ${this.stasis_app.app_name} on ${this.stasis_app.asterisk_ari.url}`);
+            this.stasis_app.asterisk_ari.ari.stop(this.stasis_app.app_name).bind(this)
+                .then(function (){
+                    console.log(`Stopped App ${this.stasis_app.app_name} on ${this.stasis_app.asterisk_ari.url}`);
+                    this.status({fill: "red", shape: "dot", text: `${this.app_name} on ${this.asterisk_ari.url}`});
+                    this.stasis_app.started = false;
+                })
+                .catch(function (err){
+                    console.error(err);
+                });
+        }
+    }
+    class AsteriskAri{
+        static GetOrCreate(url,username, password ){
+            if (url in asterisks_ari){
+                return asterisks_ari[url];
+            } else{
+                return new AsteriskAri(url,username, password);
+            }
+        }
+        constructor(url,username, password) {
+            if (url in asterisks_ari) {
+                console.log(`Asterisk Already connected!!!`);
+                //this = asterisks_ari[this.id];
+                return;
+            }
+            this.id = (url);
+            asterisks_ari[this.id] = this;
+            this.connection_status=false;
+            this.ari=null;
+            this.url= url;
+            this.username=username;
+            this.password=password;
+            this._connect_attempt();
+            setTimeout(() => {
+                            console.log(`Starting cycle for ${this.url}..`);
+                            ;
+                        }, 1000);
+        }
+
+        _connect_attempt() {
+            console.log(`Trying connect to ${this.url}`);
+            ari.connect(this.url, this.username, this.password ).bind(this)
+                .then(function (ari) {
+                    if (!this.ari){this.ari = ari;}
+                    console.log(`Connected to asterisk ${this.url}`);
+                    this.connection_status=true;
+                    this._heardbeat();
+                })
+                .catch(function (err) {
+                    console.error(`Connection to ${this.url}: ${err} trying..`);
+                    setTimeout(() => {
+                        this._connect_attempt();}, 1000);
+                });
+            }
+        _heardbeat() {
+            setTimeout(function(){
+                this.connection_status = false;
+                try {
+                    this.ari.asterisk.ping().bind(this)
+                        .then(function (asteriskping) {
+                            if (asteriskping.ping === "pong") {
+                                console.debug(`PING ${this.url} => ${asteriskping.ping}`);
+                                this.connection_status = true;
+                            } else {
+                                console.warn(`PING ${this.url} => ${asteriskping.ping}`);
+                                this.connection_status = false;
+                            }
+
+                        })
+                        .catch(function (err) {
+                            console.warn(`PING ${this.url} => ${err}`);
+                            this.connection_status = false;
+                        });
+                }
+                catch (err){
+                    this.connection_status = false;
+                    console.warn(`PING ${this.url} => ${err}`);
+                }
+                finally {
+                    this._heardbeat();
+                }
+            }.bind(this),1000);
+        }
+        /*register_app(app_name){
+            console.log(`${this.ari}, ${app_name}`);
+        }
+*/
+    }
 
     var ariConnectionPool = (function() {
         var connections = {};
-        var channels = {}
+        var channels = {};
         var obj = {
             setconn: function(url,username,password, node) {
-                var id = uuid.v4()
+                var id = uuid.v4();
                 ari.connect(url, username, password, function(err, client){
                     if (err) {
                         node.error(err);
-                        return
+                        return;
                       }
-                    var id = uuid.v4(client._connection.host)
-                    connections[id] =client
-                    clientLoaded(client, node.app_name, node,id)
+                    var id = uuid.v4(client._connection.host);
+                    connections[id] =client;
+                    clientLoaded(client, node.app_name, node,id);
                 });
                 //connections[id]._id = id;
                 //connections[id]._nodeCount = 0;
@@ -27,7 +225,7 @@ module.exports = function(RED) {
                 return connections[id];
             },
             getconn : function(id){
-                return connections[id]
+                return connections[id];
             },
             close: function(connection) {
                 connection._nodeCount -= 1;
@@ -36,56 +234,99 @@ module.exports = function(RED) {
                 }
             },
             setchan: function(channel){
-                var id = channel.id
-                channels[id] = channel
-                channel.on('StasisEnd', function (event, channel) {
+                var id = channel.id;
+                channels[id] = channel;
+                //channel.on('StasisEnd', function (event, channel) {
                     //console.log(`Channel ${channel} HANGUP`)
-                    delete channels[id]
-                })
-                return id
+                //    delete channels[id]
+                //})
+                return id;
             },
             getchan: function(id){
-                return channels[id]
+                return channels[id];
             },
             getchans: function() {
-                return channels
+                return channels;
+            },
+            closechan: function (id){
+                delete channels[id];
             }
         };
         return obj;
     }());
 
     function clientLoaded (client, app, node, id) {
-        node.status({fill:"green",shape:"dot",text:`connected to ${node.app_name}`});
+        function heardbeat() {
+            setTimeout(function(){
+                client.asterisk.ping(
+                    function (err, asteriskping) {
+                        if (err) {
+                            console.log(err);
+                            node.status({fill: "red", shape: "dot", text: `NOT connected!`});
+                        } else {
+                            if(asteriskping.ping === "pong" ){
+                                node.status({fill: "green", shape: "dot", text: `connected to ${node.app_name}`});
+                            } else {
+                                console.warn(asteriskping);
+                                node.status({fill: "orange", shape: "dot", text: `Connection Warning ${asteriskping}`});
+                            }
+
+
+                        }
+                    }
+                );
+
+
+                heardbeat();
+            },1000);
+        }
+
         function stasisStart(event, channel) {
             var dialed = event.args[0] === 'dialed';
             if (!dialed){
-                var channelid = ariConnectionPool.setchan(channel)
-                var msg = {}
-                msg.channel = channelid
-                msg.client = id
-                msg.payload = event
-                node.send([msg, null])
+                var channelid = ariConnectionPool.setchan(channel);
+                var msg = {};
+                msg.channel = channelid;
+                msg.client = id;
+                msg.payload = event;
+                var stat_data= {};
+                stat_data.chanCount = Object.keys(ariConnectionPool.getchans()).length;
+                node.send([msg, null, stat_data]);
             }
 
         }
         function stasisEnd(event, channel){
+            ariConnectionPool.closechan(channel.id);
             //console.log(event)
         }
         function dtmfEvent(event, channel){
-            var msg = {}
-            msg.channel = channel.id
-            msg.client = id
-            msg.payload = event
-            node.send([null, msg])
-
+            var msg = {};
+            msg.channel = channel.id;
+            msg.client = id;
+            msg.payload = event;
+            node.send([null, msg]);
         }
         client.on('StasisStart', stasisStart);
-        //client.on('StasisEnd', stasisEnd);
+        client.on('StasisEnd', stasisEnd);
         client.on('ChannelDtmfReceived', dtmfEvent);
         client.start(app);
+        heardbeat();
+    }
+    function ari_application_node(n){
+        RED.nodes.createNode(this,n);
+        var application_node = this;
+        var asterisk_ari_node = RED.nodes.getNode(n.server);
+        var asteriskARI_instance = AsteriskAri.GetOrCreate(asterisk_ari_node.credentials.url, asterisk_ari_node.credentials.username, asterisk_ari_node.credentials.password);
+        var stasis_app = StasisApp.GetOrCreate(n.app_name,asteriskARI_instance,application_node);
+        setTimeout(() => {
+                            console.log(`Starting cycle complete..`);
+                            ;
+                        }, 10000);
+
+
     }
 
-    function ari_client(n) {
+    /*function ari_client(n) {
         RED.nodes.createNode(this,n);
         var node = this;
         //this.sip_user = n.sip_user
@@ -102,6 +343,8 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("ari_client",ari_client);
+    */
+    RED.nodes.registerType("ari_client",ari_application_node);
 
     function ari_playback(n) {
         RED.nodes.createNode(this,n);
@@ -120,13 +363,13 @@ module.exports = function(RED) {
                     .then(function(playback) {
                         playback.on('PlaybackFinished', function (event, completedPlayback) {
                             //console.log(`Finish ${playback.id}`)
-                            msg.payload = event
-                            node.send(msg)
-                            node.status({})
+                            msg.payload = event;
+                            node.send(msg);
+                            node.status({});
                         });
                     })
                     .catch(function (err){
-                        console.error(`Playback filed ${err}`)
+                        console.error(`Playback failed ${err}`)
                         node.send(msg)
                         node.status({})
                     });
@@ -304,11 +547,11 @@ function provision(url,username,password) {
     ari.connect(url, username, password)
         .then(function (ari){
             console.log(ari.bridges);
-            console.log(ari.channels);
-            console.log(ari.endpoints);
+            //console.log(ari.channels);
+            //console.log(ari.endpoints);
         })
         .catch(function (err) {
-            console.log(err)
+            console.error(err)
         })
 }
 
@@ -366,7 +609,7 @@ function provision(url,username,password) {
 
 
 //  function deprovision(url,username,password, sip_user){
-  function deprovision(url,username,password){
+function deprovision(url,username,password){
     ari.connect(url, username, password)
     .then(function (client){
         client.asterisk.deleteObject({
